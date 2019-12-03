bccd2f575ab7de029320bc3851d12956e2e3f4d5
wangzhi <wangzhi_9211@163.com>
2019-12-03 17:52:46 +0800

    简单  上滑  下滑 点击事件 完成  回去还要再调试一下

--- app/src/androidTest/java/com/littlez/uiautomator/Uitest.java
+++ app/src/androidTest/java/com/littlez/uiautomator/Uitest.java
@@ -9,9 +9,13 @@ import junit.framework.TestCase;
 
 import org.junit.Test;
 
+import java.util.Random;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.uiautomator.UiDevice;
+import androidx.test.uiautomator.UiObject;
 import androidx.test.uiautomator.UiObjectNotFoundException;
+import androidx.test.uiautomator.UiSelector;
 
 /**
  * created by xiaozhi
@@ -30,13 +34,27 @@ public class Uitest extends TestCase {
 
         LogUtil.e("我开始运行了");
         int count = 0;
+
         try {
             while (true) {
                 LogUtil.e("我运行了" + (count++));
-                Thread.sleep(500);
-                // 点击设备返回按钮
-                uiDevice.pressBack();
 
+                Random r = new Random();
+                int number = r.nextInt(100) + 1;
+                /*随机数 进行判断 点击心或者滑动到下一个视频*/
+                if (number <= 20) {
+                    // 点击设备返回按钮
+                    uiDevice.swipe(534, 802, 400, 1500, 1);
+                    Thread.sleep(20000);
+                } else if (number <= 70) {//50的几率下滑
+                    // 点击设备返回按钮
+                    uiDevice.swipe(400, 1500, 534, 802, 1);
+                    Thread.sleep(35000);
+                } else {//30的几率点击心
+                    UiObject uiHeart = new UiObject(new UiSelector().resourceId("com.jm.video:id/image_view"));
+                    uiHeart.click();
+                    Thread.sleep(5000);
+                }
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
