b08e9491c30649bcaf115f0927cc2adb00b51108
wangzhi <wangzhi_9211@163.com>
2019-12-04 10:38:45 +0800

    刷宝短视屏的 uitest 基本很完善了

--- app/src/androidTest/java/com/littlez/uiautomator/Uitest.java
+++ app/src/androidTest/java/com/littlez/uiautomator/Uitest.java
@@ -29,6 +29,7 @@ public class Uitest extends TestCase {
         // 获取设备对象
         Instrumentation instrumentation = InstrumentationRegistry.getInstrumentation();
         UiDevice uiDevice = UiDevice.getInstance(instrumentation);
+
         // 获取上下文
         Context context = instrumentation.getContext();
 
@@ -39,28 +40,45 @@ public class Uitest extends TestCase {
             while (true) {
                 LogUtil.e("我运行了" + (count++));
 
-                Random r = new Random();
-                int number = r.nextInt(100) + 1;
-                /*随机数 进行判断 点击心或者滑动到下一个视频*/
-                if (number <= 20) {
-                    // 点击设备返回按钮
-                    uiDevice.swipe(534, 802, 400, 1200, 2);
+
+                UiObject uiHeart = uiDevice.findObject(new UiSelector().resourceId("com.jm.video:id/image_view"));
+                UiObject uiShare = uiDevice.findObject(new UiSelector().resourceId("com.jm.video:id/share"));
+
+                UiObject uiGold = uiDevice.findObject(new UiSelector().resourceId("com.jm.video:id/constraintLayout_gold"));
+                UiObject uiBalance = uiDevice.findObject(new UiSelector().resourceId("com.jm.video:id/constraintLayout_balance"));
+
+                if (uiHeart.exists() && uiShare.exists()) {//是首页
+                    Random r = new Random();
+                    int number = r.nextInt(100) + 1;
+                    /*随机数 进行判断 点击心或者滑动到下一个视频*/
+                    if (number <= 15) {//上滑
+                        uiDevice.swipe(534, 802, 400, 1200, 2);
+                        Thread.sleep(2000);
+                    } else if (number <= 70) {//下滑
+                        uiDevice.swipe(400, 1200, 534, 802, 2);
+                        Thread.sleep(10000);//播放 时长
+                    } else if (number <= 85) {//点击我的
+                        UiObject uiMe =
+                                uiDevice.findObject(new UiSelector().resourceId("com.jm.video:id/tv_tab_title").text("我"));
+                        uiMe.click();
+                        Thread.sleep(3000);
+                    } else {//30的几率点击心
+                        if (uiHeart.exists()) uiHeart.click();
+                        Thread.sleep(3000);
+                    }
+                } else if (uiGold.exists() && uiBalance.exists()) {//是我的界面
+                    UiObject uiHome =
+                            uiDevice.findObject(new UiSelector().resourceId("com.jm.video:id/tv_tab_title").text("首页"));
+                    uiHome.click();
                     Thread.sleep(2000);
-                } else if (number <= 70) {//50的几率下滑
-                    // 点击设备返回按钮
-                    uiDevice.swipe(400, 1200, 534, 802, 2);
-                    Thread.sleep(5000);
-                } else {//30的几率点击心
-                    UiObject uiHeart = new UiObject(new UiSelector().resourceId("com.jm.video:id/image_view"));
-                    uiHeart.click();
-                    Thread.sleep(5000);
-                }
+                } /*else {
+                    uiDevice.pressBack();
+                }*/
 
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
 
     }
 
