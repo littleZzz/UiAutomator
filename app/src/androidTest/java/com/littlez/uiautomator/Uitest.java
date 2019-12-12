b7eb2b59db9f18f7d8d01d8bd4f1c175d4144da4
wangzhi <wangzhi_9211@163.com>
2019-12-12 11:14:08 +0800

    处理各种异常情况下  重新激活的功能

--- app/src/androidTest/java/com/littlez/uiautomator/Uitest.java
+++ app/src/androidTest/java/com/littlez/uiautomator/Uitest.java
@@ -2,6 +2,8 @@ package com.littlez.uiautomator;
 
 import android.app.Instrumentation;
 import android.content.Context;
+import android.os.RemoteException;
+import android.util.Log;
 
 import com.littlez.uiautomator.util.LogUtil;
 
@@ -36,17 +38,18 @@ public class Uitest extends TestCase {
         LogUtil.e("我开始运行了");
         int count = 0;
 
-        try {
-            while (true) {
+        while (true) {
+
+            try {
                 LogUtil.e("我运行了" + (count++));
 
                 Thread.sleep(1500);
 
-                UiObject uiHeart = uiDevice.findObject(new UiSelector().resourceId("com.jm.video:id/image_view"));
-                UiObject uiShare = uiDevice.findObject(new UiSelector().resourceId("com.jm.video:id/share"));
+                UiObject uiHeart = new UiObject(new UiSelector().resourceId("com.jm.video:id/image_view"));
+                UiObject uiShare = new UiObject(new UiSelector().resourceId("com.jm.video:id/share"));
 
-                UiObject uiGold = uiDevice.findObject(new UiSelector().resourceId("com.jm.video:id/constraintLayout_gold"));
-                UiObject uiBalance = uiDevice.findObject(new UiSelector().resourceId("com.jm.video:id/constraintLayout_balance"));
+                UiObject uiGold = new UiObject(new UiSelector().resourceId("com.jm.video:id/constraintLayout_gold"));
+                UiObject uiBalance = new UiObject(new UiSelector().resourceId("com.jm.video:id/constraintLayout_balance"));
 
                 if (uiHeart.exists() && uiShare.exists()) {//是首页
                     Random r = new Random();
@@ -59,23 +62,47 @@ public class Uitest extends TestCase {
                         Thread.sleep(8000);//播放 时长
                     } else if (number <= 90) {//点击我的
                         UiObject uiMe =
-                                uiDevice.findObject(new UiSelector().resourceId("com.jm.video:id/tv_tab_title").text("我"));
+                                new UiObject(new UiSelector().resourceId("com.jm.video:id/tv_tab_title").text("我"));
                         uiMe.click();
                     } else {//3点击心
                         if (uiHeart.exists()) uiHeart.click();
                     }
                 } else if (uiGold.exists() && uiBalance.exists()) {//是我的界面
                     UiObject uiHome =
-                            uiDevice.findObject(new UiSelector().resourceId("com.jm.video:id/tv_tab_title").text("首页"));
+                            new UiObject(new UiSelector().resourceId("com.jm.video:id/tv_tab_title").text("首页"));
                     uiHome.click();
-                } /*else {
-                    uiDevice.pressBack();
-                }*/
+                } else {//处理异常情况  1.0 点击重播 2.0 广告滑动一下
 
+                    uiDevice.pressHome();
+                    Thread.sleep(200);
+                    uiDevice.pressRecentApps();
+                    Thread.sleep(200);
+                    UiObject appLaunch = new UiObject(new UiSelector().text("刷宝短视频"));
+                    if (appLaunch.exists()) {//没有彻底挂掉
+
+                        appLaunch.click();
+                        Thread.sleep(500);
+                        uiDevice.swipe(400, 1200, 534, 802, 2);
+                        Thread.sleep(1000);
+
+                    } else {//彻底挂掉了  重启
+                        uiDevice.pressHome();
+                        Thread.sleep(200);
+                        //启动应用
+                        UiObject uiVideo = new UiObject(new UiSelector().text("刷宝短视频"));
+                        if (uiVideo.exists()) {
+                            uiVideo.click();
+                            Thread.sleep(2000);
+                        }
+                    }
+
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
+
 
     }
 
