10e73eec6542a0f0f24b770e90f45dcb7c25f176
littleZ <wangzhi_9211@163.com>
2020-03-28 17:13:32 +0800

    东方头条  查找id慢  实在无法解决

--- app/src/androidTest/java/com/littlez/uiautomator/A10DongFangTTtest.java
+++ app/src/androidTest/java/com/littlez/uiautomator/A10DongFangTTtest.java
@@ -31,8 +31,8 @@ public class A10DongFangTTtest extends TestCase {
         Instrumentation instrumentation = InstrumentationRegistry.getInstrumentation();
         UiDevice uiDevice = UiDevice.getInstance(instrumentation);
 
-        A00UtilTest.baseMethod(uiDevice, 0, appName);//启动时  先关闭其他的
-        A00UtilTest.errorCount = 0;//重置次数
+//        A00UtilTest.baseMethod(uiDevice, 0, appName);//启动时  先关闭其他的
+//        A00UtilTest.errorCount = 0;//重置次数
         try {
             while (true) {
                 //主页
@@ -40,24 +40,23 @@ public class A10DongFangTTtest extends TestCase {
                 if (uiHome.exists()) {//是主页  滑动选择条目
                     UiObject uiHomeTitle = new UiObject(new UiSelector().resourceId("com.songheng.eastnews:id/afj"));
                     if (uiHomeTitle.exists()) {//选中的是首页
-
-                        if (isFirstRun) {//第一次启动要刷新一下数据
-                            UiObject uinews = new UiObject(new UiSelector().resourceId("com.songheng.eastnews:id/a5t"));
-                            if (uinews.exists()) {
-                                uinews.click();
-                                isFirstRun = false;
-                                Thread.sleep(2000);
-                                continue;
-                            }
-                        }
+//                        if (isFirstRun) {//第一次启动要刷新一下数据
+//                            UiObject uinews = new UiObject(new UiSelector().resourceId("com.songheng.eastnews:id/a5t"));
+//                            if (uinews.exists()) {
+//                                uinews.click();
+//                                isFirstRun = false;
+//                                Thread.sleep(2000);
+//                                continue;
+//                            }
+//                        }
                         //主页滑动选择条目
-                        Random r = new Random();
-                        int number = r.nextInt(100) + 1;
-                        if (number <= 2) {//上滑
-                            A00UtilTest.swipUp(uiDevice, 12);
-                        } else if (number <= 95) {//下滑
-                            A00UtilTest.swipDown(uiDevice, 12);
-                        } //做一些其他额外的附加任务
+//                        Random r = new Random();
+//                        int number = r.nextInt(100) + 1;
+//                        if (number <= 2) {//上滑
+//                            A00UtilTest.swipUp(uiDevice, 12);
+//                        } else if (number <= 95) {//下滑
+//                            A00UtilTest.swipDown(uiDevice, 12);
+//                        } //做一些其他额外的附加任务
                         //首页领取  做一个时段任务奖励的领取
                         UiObject uiGet = new UiObject(new UiSelector()
                                 .resourceId("com.songheng.eastnews:id/aj9").text("领取"));
@@ -75,23 +74,22 @@ public class A10DongFangTTtest extends TestCase {
                         }
                         Thread.sleep(1500);//要听一下  给一些加载时间
                     }
-
                 } else {//去检测是不是我想要的界面  是就进行处理
                     UiObject uiListView = new UiObject(new UiSelector().resourceId("com.songheng.eastnews:id/ax3"));//列表
-                    UiObject uiCollect = new UiObject(new UiSelector().resourceId("com.songheng.eastnews:id/a12"));//收藏
+                    UiObject uiNewList = new UiObject(new UiSelector().resourceId("com.songheng.eastnews:id/a37"));//新闻列表
                     LogUtil.e("1");
-                    if (uiCollect.exists()) {//是新闻
+                    if (uiNewList.exists()) {//是新闻
                         LogUtil.e("2");
                         boolean isRun = true;
                         while (isRun) {
                             LogUtil.e("3");
                             UiObject uiHotNews = new UiObject(new UiSelector().resourceId("com.songheng.eastnews:id/aim"));//热门新闻
-                            UiObject uiMore = new UiObject(new UiSelector().resourceId("com.songheng.eastnews:id/a4y"));//这个查找慢
+                            UiObject uiMore = new UiObject(
+                                    new UiSelector().resourceIdMatches("com.songheng.eastnews:id/a4y"));//这个查找慢
                             LogUtil.e("4");
                             if (uiMore.exists()) {//查看更多
                                 LogUtil.e("5");
                                 uiMore.click();
-                                Thread.sleep(500);
                             } else if (uiHotNews.exists()) {
                                 LogUtil.e("6");
                                 isRun = false;
