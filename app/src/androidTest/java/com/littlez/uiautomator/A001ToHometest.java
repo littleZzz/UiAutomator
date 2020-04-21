34425774eac52859ecf80bcd0a53bbeb9f935d01
littleZ <wangzhi_9211@163.com>
2020-04-22 07:21:31 +0800

    自动调用豌豆荚清理内存  闪电盒子清除 每日爱清理加入

--- app/src/androidTest/java/com/littlez/uiautomator/A001ToHometest.java
+++ app/src/androidTest/java/com/littlez/uiautomator/A001ToHometest.java
@@ -21,18 +21,43 @@ import java.util.Random;
  */
 public class A001ToHometest extends TestCase {
 
+    /*app 名字*/
+    private String appName = "豌豆荚";
+    private boolean appRun = true;//appRun
+
     //    @Test
     public void test() throws UiObjectNotFoundException {
         // 获取设备对象
         Instrumentation instrumentation = InstrumentationRegistry.getInstrumentation();
         UiDevice uiDevice = UiDevice.getInstance(instrumentation);
-        int count = 0;
+
+        A00UtilTest.baseMethod(uiDevice, 0, appName, null);//启动时  先关闭其他的
         try {
-            while (count <= 3) {
+            while (appRun) {
                 UiObject uiHomeLive = new UiObject(new UiSelector().resourceId("com.android.launcher3:id/live"));
-                if (uiHomeLive.exists()) {
-                    count++;
+                UiObject uiMe = new UiObject(new UiSelector().resourceId("android:id/text1").text("我的"));
+                UiObject uiGC = new UiObject(new UiSelector().resourceId("com.wandoujia.phoenix2:id/ke").text("垃圾清理"));
+                UiObject uiGCOver = new UiObject(new UiSelector().resourceId("com.wandoujia.phoenix2:id/vo").text("已清理"));
+
+                if (uiHomeLive.exists()) {//在手机主页
+                    //启动豌豆荚
+                    UiObject uiVideo = new UiObject(new UiSelector().text(appName));
+                    if (uiVideo.exists()) {
+                        uiVideo.click();
+                        Thread.sleep(10000);
+                    }
+                } else if (uiMe.exists() && !uiMe.isSelected()) {//在豌豆荚
+                    uiMe.click();
+                } else if (uiGC.exists()) {//垃圾清理
+                    uiGC.click();
+                    UiObject uiOneTimesGC =
+                            new UiObject(new UiSelector().resourceId("com.wandoujia.phoenix2:id/v6").textContains("一键清理"));
+                    A00UtilTest.backUntilObjOrTime(uiDevice, uiOneTimesGC, 50);
+                } else if (uiGCOver.exists()) {//已清理界面
+                    uiDevice.pressHome();
+                    break;
                 } else {
+                    A00UtilTest.baseMethod(uiDevice, 0, appName, null);//启动时  先关闭其他的
                     uiDevice.pressHome();
                 }
                 Thread.sleep(5000);
---
+++ app/src/androidTest/java/com/littlez/uiautomator/A08MeiRiAiQingLitest.java
@@ -0,0 +1,89 @@
+package com.littlez.uiautomator;
+
+import android.app.Instrumentation;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.uiautomator.UiDevice;
+import androidx.test.uiautomator.UiObject;
+import androidx.test.uiautomator.UiObjectNotFoundException;
+import androidx.test.uiautomator.UiSelector;
+
+import junit.framework.TestCase;
+
+import java.util.Random;
+
+/**
+ * created by xiaozhi
+ * <p>每日爱清理
+ * Date 2019/12/3
+ */
+public class A08MeiRiAiQingLitest extends TestCase {
+
+    /*app 名字*/
+    private String appName = "每日爱清理";
+    private boolean appRun = true;//appRun
+
+    //    @Test
+    public void test() throws UiObjectNotFoundException {
+        // 获取设备对象
+        Instrumentation instrumentation = InstrumentationRegistry.getInstrumentation();
+        UiDevice uiDevice = UiDevice.getInstance(instrumentation);
+
+        try {
+            A00UtilTest.baseMethod(uiDevice, 0, appName, null);//启动时  先关闭其他的
+            A00UtilTest.errorCount = 0;//重置错误次数
+
+            while (appRun) {
+                UiObject uiMainTitle = new UiObject(new UiSelector().resourceId("com.xiaoqiao.qclean:id/tv_main_title"));
+                UiObject uiTvHeart = new UiObject(new UiSelector().resourceId("com.xiaoqiao.qclean:id/tv_like"));//
+                UiObject uiRecycleView = new UiObject(new UiSelector().resourceId("com.xiaoqiao.qclean:id/community_recycler_view"));//外层recycleView
+                UiObject uiRewardButton = new UiObject(new UiSelector().resourceId("com.xiaoqiao.qclean:id/tv_gold_double"));//看广告获取更多金币
+
+                if (uiMainTitle.exists()) {//在首页
+                    UiObject uiTV = new UiObject(new UiSelector().resourceId("com.xiaoqiao.qclean:id/ll_video"));
+                    uiTV.click();
+                } else if (uiTvHeart.exists() || uiRecycleView.exists()) {//是视频界面
+                    Random r = new Random();
+                    int number = r.nextInt(100) + 1;
+                    /*随机数 进行判断 点击心或者滑动到下一个视频*/
+                    if (number <= 3) {//上一条
+                        A00UtilTest.swipUp(uiDevice);
+                    } else if (number <= 97) {//下一条
+                        A00UtilTest.swipDown(uiDevice);
+                        Random rr = new Random();
+                        Thread.sleep((15 + rr.nextInt(15) + 1) * 1000);//播放 时长
+                    } else if (number <= 100) {//点击任务
+                        if (uiTvHeart.exists()) uiTvHeart.click();
+                    }
+                } else if (uiRewardButton.exists()) {//看广告获取更多
+                    uiRewardButton.click();
+                    A00UtilTest.backUntilObjOrTime(uiDevice, null, 55);
+                    Thread.sleep(2000);
+                } else {//处理异常情况
+                    UiObject uiDialog = new UiObject(new UiSelector().resourceId("com.xiaoqiao.qclean:id/tv_close"));
+                    UiObject uiDialog02 = new UiObject(new UiSelector().resourceId("com.xiaoqiao.qclean:id/ib_close_btn"));
+                    UiObject uiWebView = new UiObject(new UiSelector().className("android.webkit.WebView").descriptionContains("腾讯社交联盟广告"));
+                    if (uiDialog.exists()) {//暂不领取
+                        uiDialog.click();
+                    } else if (uiDialog02.exists()) {//获得金币dia
+                        uiDialog02.click();
+                    } else if (uiWebView.exists()) {//广告页面
+                        uiDevice.pressBack();
+                    } else {//最终的强制搞一波
+                        A00UtilTest.baseMethod(uiDevice, 1, appName, new A00UtilTest.MyCallBack() {
+                            @Override
+                            public void callback(boolean isStop) {
+                                appRun = false;//出问题了停止运行
+                            }
+                        });
+                    }
+                }
+                Thread.sleep(1000);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
