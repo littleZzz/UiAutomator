ee3080154725c99b72eac925b2e0651cc3cd03af
littleZ <wangzhi_9211@163.com>
2020-04-27 06:59:46 +0800

    提交

--- app/src/androidTest/java/com/littlez/uiautomator/A04YinLangtest.java
+++ app/src/androidTest/java/com/littlez/uiautomator/A04YinLangtest.java
@@ -8,6 +8,8 @@ import androidx.test.uiautomator.UiObject;
 import androidx.test.uiautomator.UiObjectNotFoundException;
 import androidx.test.uiautomator.UiSelector;
 
+import com.littlez.uiautomator.util.LogUtil;
+
 import junit.framework.TestCase;
 
 import java.util.Random;
@@ -30,15 +32,22 @@ public class A04YinLangtest extends TestCase {
         UiDevice uiDevice = UiDevice.getInstance(instrumentation);
 
         try {
-            A00UtilTest.baseMethod(uiDevice, 0, appName, null);//启动时  先关闭其他的
-            A00UtilTest.errorCount = 0;//重置错误次数
+//            A00UtilTest.baseMethod(uiDevice, 0, appName, null);//启动时  先关闭其他的
+//            A00UtilTest.errorCount = 0;//重置错误次数
 
             while (appRun) {
-                UiObject uiWithdraw = new UiObject(new UiSelector().resourceId("com.video.yl:id/layout_withdraw"));//提现栏
+                UiObject uiHome = new UiObject(new UiSelector().resourceId("com.video.yl:id/view_pager"));//提现栏
                 UiObject uiGoWithdraw = new UiObject(new UiSelector().resourceId("com.video.yl:id/go_withdraw"));//我的 金钱界面
                 UiObject uiRewardButton = new UiObject(new UiSelector().resourceId("com.video.yl:id/tv_rewardButton"));//看广告获取更多金币
 
-                if (uiWithdraw.exists()) {//是首页
+                LogUtil.e("111");
+                if (uiHome.exists()) {
+                    LogUtil.e("有");
+                } else {
+                    LogUtil.e("meiyou ");
+                }
+                if (uiHome.exists()) {//是首页
+                    LogUtil.e("2");
                     Random r = new Random();
                     int number = r.nextInt(100) + 1;
                     /*随机数 进行判断 点击心或者滑动到下一个视频*/
@@ -50,20 +59,24 @@ public class A04YinLangtest extends TestCase {
                         Thread.sleep((40 + rr.nextInt(15) + 1) * 1000);//播放 时长
                     } else if (number <= 100) {//点击我的
                         UiObject uiMe = new UiObject(new UiSelector().resourceId("com.video.yl:id/tab_text").text("我"));
-                        uiMe.click();
+                        if (uiMe.exists()) uiMe.click();
                     }
                 } else if (uiGoWithdraw.exists()) {//是我的界面
-                    UiObject uiHome = new UiObject(new UiSelector().resourceId("com.video.yl:id/tab_text").text("首页"));
-                    uiHome.click();
+                    if (uiHome.exists()) uiHome.click();
                 } else if (uiRewardButton.exists()) {//看广告获取更多
+                    LogUtil.e("4");
                     uiRewardButton.click();
                     UiObject uiAdvClose = new UiObject(new UiSelector().resourceId("com.video.yl:id/tt_video_ad_close_layout"));
                     UiObject uiAdvClose02 = new UiObject(new UiSelector().resourceId("com.video.yl:id/reward_ad_close"));
                     A00UtilTest.backUntilObjOrTime(uiDevice, uiAdvClose, uiAdvClose02, 50);
                 } else {//处理异常情况
-                    UiObject uiPrivacy = new UiObject(new UiSelector().resourceId("sdf"));
-                    if (uiPrivacy.exists()) {//用户协议
-                        uiPrivacy.click();
+                    LogUtil.e("3");
+                    UiObject uiadv = new UiObject(new UiSelector().resourceId("com.video.yl:id/tt_video_ad_close_layout"));
+                    UiObject uiadv02 = new UiObject(new UiSelector().resourceId("com.video.yl:id/reward_ad_close"));
+                    if (uiadv.exists()) {//用户协议
+                        uiadv.click();
+                    } else if (uiadv02.exists()) {//用户协议
+                        uiadv02.click();
                     } else {//最终的强制搞一波
                         A00UtilTest.baseMethod(uiDevice, 1, appName, new A00UtilTest.MyCallBack() {
                             @Override
